4101fbef83a3190d01d6d2e678ff3f292e543ce1
Radu Ioan Fericean <radu@fericean.ro>
2014-10-16 20:13:57 +0300

    first version

---
+++ controllers/default.py
@@ -0,0 +1,71 @@
+ # -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+from trade_checker import trade_checker
+
+def index():
+    form = SQLFORM(Trade)
+    if form.process().accepted:
+        if trade_checker(form.vars, db(PortofolioRule).select()):
+            Trade(form.vars.id).update_record(compliant=True)
+            session.flash = 'trade inserted'
+            redirect('good_boy')
+        else:
+            session.flash = 'trade rejected'
+            redirect('bad_boy')
+    return dict(form=form)
+
+def good_boy():
+    return dict()
+
+def bad_boy():
+    return dict()
+
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    http://..../[app]/default/user/manage_users (requires membership in
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    """
+    return dict(form=auth())
+
+
+@cache.action()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
+
+
+def call():
+    """
+    exposes services. for example:
+    http://..../[app]/default/call/jsonrpc
+    decorate with @services.jsonrpc the functions to expose
+    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+    """
+    return service()
+
+
+@auth.requires_login() 
+def api():
+    """
+    this is example of API with access control
+    WEB2PY provides Hypermedia API (Collection+JSON) Experimental
+    """
+    from gluon.contrib.hypermedia import Collection
+    rules = {
+        '<tablename>': {'GET':{},'POST':{},'PUT':{},'DELETE':{}},
+        }
+    return Collection(db).process(request,response,rules)
